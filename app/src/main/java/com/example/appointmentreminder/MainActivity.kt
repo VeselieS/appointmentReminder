5194a68a84e5e012ca83b8287fba84fc6b2e9a0b
ripperino <vadik.volkorezov@yandex.ru>
2024-06-03 21:21:12 +1000

    Initial commit

---
+++ app/src/main/java/com/example/appointmentreminder/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.appointmentreminder
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val addAppointment: FloatingActionButton = findViewById<FloatingActionButton>(R.id.fabAdd)
+
+        addAppointment.setOnClickListener() {
+            val intent = Intent(this, AddAppointment::class.java)
+            startActivity(intent)
+        }
+    }
+}
