92260ef004fdd240edd44cc39644a2322b3d8c40
ripperino <vadik.volkorezov@yandex.ru>
2024-06-19 12:09:21 +1000

    SavePoint

--- app/src/main/java/com/example/appointmentreminder/MainActivity.kt
+++ app/src/main/java/com/example/appointmentreminder/MainActivity.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
